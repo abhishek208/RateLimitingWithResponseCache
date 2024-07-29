d86d670e65a4bff0916d5a4f2563b22b5b66a3c8
User <User@DESKTOP-0LM06S8>
2024-07-29 16:17:37 +0530

    Add project files.

---
+++ RateLimitingWithResponseCache/Program.cs
@@ -0,0 +1,17 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddResponseCaching();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseResponseCaching();
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
